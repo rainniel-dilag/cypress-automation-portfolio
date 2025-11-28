9d2fdfb6f28f380681a24b6a44da0d6b61fee2dd
Rainniel Dilag <internwebdev@highlysucceed.com>
2025-11-29 01:08:51 +0800

    Added viewport testing for tablet and desktop

--- cypress/e2e/viewport.cy.js
+++ cypress/e2e/viewport.cy.js
@@ -3,25 +3,73 @@ describe('Viewport & Responsiveness Tests', () => {
     cy.loginAndVisitInventoryPage()
   })
 
-  const mobileSizes = ['iphone-5', 'iphone-xr', 'samsung-note9']
+  describe('Mobile Viewports', () => {
+    const mobileSizes = ['iphone-5', 'iphone-x', 'samsung-note9']
 
-  mobileSizes.forEach((phone) => {
-    it(`Test viewports of mobile: ${phone}`, () => {
-      cy.viewport(phone)
-      cy.get('.bm-burger-button').should('be.visible').and('not.disabled').click()
-      cy.get('.bm-item-list').should('be.visible')
-      cy.get('.bm-cross-button').should('be.visible').and('not.disabled').click()
-      cy.get('.bm-item-list').should('not.be.visible')
-      cy.getDataTest('inventory-list').should('be.visible')
-      cy.getDataTest('shopping-cart-link').should('be.visible').and('not.disabled')
+    mobileSizes.forEach((phone) => {
+      it(`Test viewports of mobile: ${phone}`, () => {
+        cy.viewport(phone)
+        cy.get('.bm-burger-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('be.visible')
+        cy.get('.bm-cross-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('not.be.visible')
+        cy.getDataTest('inventory-list').should('be.visible')
+        cy.getDataTest('shopping-cart-link').should('be.visible').and('not.disabled')
 
-      cy.getDataTest('inventory-item').each(($item) => {
-        cy.wrap($item).first().within(() => {
-          cy.get('[data-test^="add-to-cart"]').should('be.visible').and('not.disabled')
+        cy.getDataTest('inventory-item').each(($item) => {
+          cy.wrap($item).within(() => {
+            cy.get('[data-test^="add-to-cart"]').should('be.visible').and('not.disabled')
+          })
+        })
+      })
+    })
+  })
+
+  describe('Tablet Viewports', () => {
+    const tabletDevices= ['ipad-mini', 'ipad-2']
+
+    tabletDevices.forEach((tablet) => {
+      it(`Test viewports of mobile: ${tablet}`, () => {
+        cy.viewport(tablet)
+        cy.get('.bm-burger-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('be.visible')
+        cy.get('.bm-cross-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('not.be.visible')
+        cy.getDataTest('inventory-list').should('be.visible')
+        cy.getDataTest('shopping-cart-link').should('be.visible').and('not.disabled')
+
+        cy.getDataTest('inventory-item').each(($item) => {
+          cy.wrap($item).within(() => {
+            cy.get('[data-test^="add-to-cart"]').should('be.visible').and('not.disabled')
+          })
+        })
+      })
+    })
+  })
+
+  describe('Desktop Viewports', () => {
+    const desktopDevices= ['macbook-11', 'macbook-13', 'macbook-15']
+
+    desktopDevices.forEach((desktop) => {
+      it(`Test viewports of desktop: ${desktop}`, () => {
+        cy.viewport(desktop)
+        cy.get('.bm-burger-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('be.visible')
+        cy.get('.bm-cross-button').should('be.visible').and('not.disabled').click()
+        cy.get('.bm-item-list').should('not.be.visible')
+        cy.getDataTest('inventory-list').should('be.visible')
+        cy.getDataTest('shopping-cart-link').should('be.visible').and('not.disabled')
+
+        cy.getDataTest('inventory-item').each(($item) => {
+          cy.wrap($item).within(() => {
+            cy.get('[data-test^="add-to-cart"]').should('be.visible').and('not.disabled')
+          })
         })
       })
     })
   })
 })
+
+
 
 
